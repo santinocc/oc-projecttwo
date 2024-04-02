710b4321a9cb509e67744f3b678e2b32d4f60b13
Santino <santino.cervera@gmail.com>
2024-04-02 18:30:04 +0900

    04.02.2024 Git Project Two Initial Setup

---
+++ src/main/java/com/oc/projecttwo/ProjecttwoApplication.java
@@ -0,0 +1,13 @@
+package com.oc.projecttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/projecttwo/ProjecttwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.projecttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
