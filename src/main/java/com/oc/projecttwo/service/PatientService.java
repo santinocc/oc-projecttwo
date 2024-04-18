c5ab853f432a6bbbdea8d7ae2a358924bc75a1b5
Santino <santino.cervera@gmail.com>
2024-04-18 17:43:46 +0900

    04.18.24 Patient Update Method Fixed / Tests Pending

--- src/main/java/com/oc/projecttwo/service/PatientService.java
+++ src/main/java/com/oc/projecttwo/service/PatientService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 public class PatientService {
@@ -28,26 +29,44 @@ public class PatientService {
     
 	public boolean update(Patient patientUpdate) {
 		
-		List<Patient> patients = patientRepository.findAll();
-        boolean isUpdated = false;
+		Optional<Patient> patientToUpdate = patientRepository.findById(patientUpdate.getId());
+//		List<Patient> patients = patientRepository.findAll();
+//        boolean isUpdated = false;
+        AtomicBoolean isUpdated = new AtomicBoolean(false);
 
-        for (Patient patient : patients) {
-            if (patient.family.equals(patientUpdate.family) && patient.given.equals(patientUpdate.given)) {
+        patientToUpdate.ifPresent(p-> {
+        	p.family = patientUpdate.family;
+        	p.given = patientUpdate.given;
+            p.dob = patientUpdate.dob;
+            p.sex = patientUpdate.sex;
+            p.address = patientUpdate.address;
+            p.phone = patientUpdate.phone;
+            
+//            isUpdated = true;
+            isUpdated.set(true);
+        });
+        
+        if (isUpdated.get()) { 
+        	patientRepository.save(patientToUpdate.get()); 
+        }
+        
+//        if (patientToUpdate !=null) {
+//        for (Patient patient : patients) {
+//            if (patient.family.equals(patientUpdate.family) && patient.given.equals(patientUpdate.given)) {
 //                patient.setDob(patientUpdate.dob);
 //                patient.setSex(patientUpdate.sex);
 //                patient.setAddress(patientUpdate.address);
 //                patient.setPhone(patientUpdate.phone);
             	
-                patient.dob = patientUpdate.dob;
-                patient.sex = patientUpdate.sex;
-                patient.address = patientUpdate.address;
-                patient.phone = patientUpdate.phone;
+//        patientToUpdate.dob = patientUpdate.dob;
+//        patientToUpdate.sex = patientUpdate.sex;
+//        patientToUpdate.address = patientUpdate.address;
+//        patientToUpdate.phone = patientUpdate.phone;
 
-                isUpdated = true;
-                break;
-            }
-        }
-        return isUpdated;
+//                break;
+//            }
+//        }
+        return isUpdated.get();
 	}
 
     public void deleteById(Long id) {
